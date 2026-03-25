09ff42f0a029d07212f33e1cb929a6b9363c59c9
virzarasydann <virzarasydan@gmail.com>
2026-03-25 09:59:53 +0700

    menambahkan class DataGeneric

---
+++ tpModul5_103082400034/DataGeneric.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tpModul5_103082400034
+{
+    internal class DataGeneric<T>
+    {
+        public T Data;
+        public DataGeneric(T data)
+        {
+
+            Data = data;
+        }
+
+        public  void PrintData()
+        {
+
+
+            Console.WriteLine("Data yang dipanggil adalah : " + Data);
+        }
+    }
+}
