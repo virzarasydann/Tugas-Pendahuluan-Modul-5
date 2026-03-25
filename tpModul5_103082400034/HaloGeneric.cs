ac2798ae990285051c84a07dc5456a44772cdcc6
virzarasydann <virzarasydan@gmail.com>
2026-03-25 09:40:42 +0700

    menambahkan class HaloGeneric dengan method

---
+++ tpModul5_103082400034/HaloGeneric.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tpModul5_103082400034
+{
+    internal class HaloGeneric
+    {
+        public void SapaUser<T>(T data)
+        {
+
+            Console.WriteLine($"Halo User: {data}");
+        }
+    }
+}
